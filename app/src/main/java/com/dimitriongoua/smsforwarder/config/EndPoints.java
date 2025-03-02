3141c01ce192fd0dac66bc0c78070e622c1169c3
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2025-03-02 18:00:11 +0100

    Update webhook endpoint

--- app/src/main/java/com/dimitriongoua/smsforwarder/config/EndPoints.java
+++ app/src/main/java/com/dimitriongoua/smsforwarder/config/EndPoints.java
@@ -2,5 +2,5 @@ package com.dimitriongoua.smsforwarder.config;
 
 public class EndPoints {
     public static final String BOT_URL = "https://api.telegram.org/bot{BOT_TOKEN}/sendMessage";
-    public static final String WEBHOOK_URL = "https://miango.netlify.app/.netlify/functions/smshandler";
+    public static final String WEBHOOK_URL = "https://miango.netlify.app/smshandler";
 }
