0a6a8b514c01be9f3591e9bd50ed0945a26363a3
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2025-03-02 18:55:22 +0000

    Add env var in code

--- app/src/main/java/com/dimitriongoua/smsforwarder/config/Constants.java
+++ app/src/main/java/com/dimitriongoua/smsforwarder/config/Constants.java
@@ -1,7 +1,7 @@
 package com.dimitriongoua.smsforwarder.config;
 
 public class Constants {
-    public static final String BOT_TOKEN = "xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxx";
-    public static final String CHAT_ID = "xxxxxxxxxx";
+    public static final String BOT_TOKEN = BuildConfig.BOT_TOKEN;
+    public static final String CHAT_ID = BuildConfig.CHAT_ID;
     public static final String KEY_SMS = "sms_body";
 }
